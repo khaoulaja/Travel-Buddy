b7807abe97e4e3dadbdd14e3af171db0fc565732
Javarris Barnett <javarrisb@gmail.con>
2021-11-28 19:07:27 +0100

    Added api html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,149 @@
-var countryEl = document.querySelector("#please");
+var areaName = document.querySelector("#area");
+var capitalName = document.querySelector("#capital");
+var langaugeName = document.querySelector("#language");
+var currencyName = document.querySelector("#currency");
+var areaNameOne = document.querySelector("#area1");
+var capitalNameOne = document.querySelector("#capital1");
+var languageNameOne = document.querySelector("#language1");
+var currencyNameOne = document.querySelector("#currency1");
+var areaNameTwo = document.querySelector("#area2");
+var capitalNameTwo = document.querySelector("#capital2");
+var languageNameTwo = document.querySelector("#language2");
+var currencyNameTwo = document.querySelector("#currency2");
+var areaNameThree = document.querySelector("#area3");
+var capitalNameThree = document.querySelector("#capital3");
+var languageNameThree = document.querySelector("#language3");
+var currencyNameThree = document.querySelector("#currency3");
+var areaNameFour = document.querySelector("#area4");
+var capitalNameFour = document.querySelector("#capital4");
+var languageNameFour = document.querySelector("#language4");
+var currencyNameFour = document.querySelector("#currency4");
+
 var apiUrl = "https://restcountries.com/v3.1/all"
+var bpiUrl = "https://libretranslate.com/";
 
-var getCountries = function () {
+var getSpain = function () {
     fetch(apiUrl)
-        .then(function (response) {
-            response.json().then(function (data) {
-                var elem = document.getElementById("please");
+        .then(res => res.json())
+        .then(data => {
 
-                $("elem").data("name");
-            });
+            var array = data[102].area;
+            console.log(data);
+            var cap = data[102].capital[0];
+            var diction = data[102].languages['spa'];
+            console.log(diction);
+            var money = data[102].currencies['EUR'].name;
+            console.log(money);
+
+            areaName.innerHTML = `Area: ${array}`;
+            capitalName.innerHtml = `Capital: ${cap}`;
+            langaugeName.innerHTML = `Language: ${diction}`
+            currencyName.innerHtml = `Currency: ${money}`
+
         });
-}
+};
+
+var getGermany = function () {
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+
+            var array1 = data[174].area;
+            console.log(array1)
+            var cap1 = data[174].capital[0];
+            var diction1 = data[174].languages['deu'];
+
+            var money1 = data[174].currencies['EUR'].name;
+            console.log(money1);
+
+            areaNameOne.innerHTML = `Area: ${array1}`;
+            capitalNameOne.innerHtml = `Capital: ${cap1}`;
+            languageNameOne.innerHTML = `Language: ${diction1}`
+            currencyNameOne.innerHtml = `Currency: ${money1}`
+
+        });
+};
 
 
+
+var getFrance = function () {
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+
+            var array2 = data[120].area;
+            console.log(array2)
+            var cap2 = data[120].capital[0];
+            var diction2 = data[120].languages['fra'];
+
+            var money2 = data[120].currencies['EUR'].name;
+            console.log(money2);
+
+            areaNameTwo.innerHTML = `Area: ${array2}`;
+            capitalNameTwo.innerHtml = `Capital: ${cap2}`;
+            languageNameTwo.innerHTML = `Language: ${diction2}`
+            currencyNameTwo.innerHtml = `Currency: ${money2}`
+
+        });
+};
+
+
+
+var getItaly = function () {
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+
+            var array3 = data[65].area;
+            console.log(array3)
+            var cap3 = data[165].capital[0];
+            var diction3 = data[165].languages['ita'];
+
+            var money3 = data[165].currencies['EUR'].name;
+            console.log(money3);
+
+            areaNameThree.innerHTML = `Area: ${array3}`;
+            capitalNameThree.innerHtml = `Capital: ${cap3}`;
+            languageNameThree.innerHTML = `Language: ${diction3}`
+            currencyNameThree.innerHtml = `Currency: ${money3}`
+
+        });
+};
+
+var getNether = function () {
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+
+            var array4 = data[241].area;
+            console.log(array4)
+            var cap4 = data[241].capital[0];
+            var diction4 = data[241].languages['nld'];
+
+            var money4 = data[241].currencies['EUR'].name;
+            console.log(money4);
+
+            areaNameFour.innerHTML = `Area: ${array4}`;
+            capitalNameFour.innerHtml = `Capital: ${cap4}`;
+            languageNameFour.innerHTML = `Language: ${diction4}`
+            currencyNameFour.innerHtml = `Currency: ${money4}`
+
+        });
+};
 
 
 
 var getTranslation = function () {
-    var bpiUrl = "https://libretranslate.com/spec";
-
-    fetch(apiUrl)
-        .then(function (response) {
-            response.json().then(function (data) {
-                
-            });
-        });
+    fetch(bpiUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+})
 }
 
-var displayCountries = function() {
-
-}
-getCountries();
+getSpain();
+getGermany();
+getFrance();
+getItaly();
+getNether();
 getTranslation();
